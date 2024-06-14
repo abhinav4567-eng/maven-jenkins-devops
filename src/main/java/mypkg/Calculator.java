a39ffb23cc70c5d0331b3fcbe27225dd9eab66e3
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2024-06-14 12:25:45 +0530

    div func added

--- src/main/java/mypkg/Calculator.java
+++ src/main/java/mypkg/Calculator.java
@@ -13,13 +13,19 @@ public class Calculator {
 	public int mul(int a, int b) {
 		return a*b;
 	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator ob = new Calculator();
 		System.out.println("Sum is "+ob.sum(20, 10));
 		System.out.println("Sub is "+ob.sub(20, 10));
 		System.out.println("Mul is "+ob.mul(20, 10));
-
+		System.out.println("Div is "+ob.div(20, 10));
+        
 	}
 
 }
