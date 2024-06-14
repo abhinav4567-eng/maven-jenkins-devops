a990f65de8bd7a0c9118946a15c19a10843fb776
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2024-06-14 12:19:10 +0530

    mul is added

--- src/main/java/mypkg/Calculator.java
+++ src/main/java/mypkg/Calculator.java
@@ -9,11 +9,16 @@ public class Calculator {
 	public int sub(int a, int b) {
 	       return a-b;
 	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator ob = new Calculator();
 		System.out.println("Sum is "+ob.sum(20, 10));
 		System.out.println("Sub is "+ob.sub(20, 10));
+		System.out.println("Mul is "+ob.mul(20, 10));
 
 	}
 
