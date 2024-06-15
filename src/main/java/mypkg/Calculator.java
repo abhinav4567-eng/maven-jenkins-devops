539a8b505a8afe6cb72586856f9eea87274288e6
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2024-06-15 11:23:15 +0530

    test cases resolved

--- src/main/java/mypkg/Calculator.java
+++ src/main/java/mypkg/Calculator.java
@@ -15,11 +15,11 @@ public class Calculator {
 	}
 	
 	public int div(int a, int b) {
-		return a+b;
+		return a/b;
 	}
 	
 	public int mod(int a, int b) {
-		return a-b;
+		return a%b;
 	}
 	
 	public void display() {
