4d0a3e1bcca3c19f133ce9e8528ce31b0c09269a
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2024-06-16 10:21:55 +0530

    sonarqube plugin added!

---
+++ src/main/java/mypkg/SonarTest.java
@@ -0,0 +1,21 @@
+package mypkg;
+
+public class SonarTest {
+	
+	public int sum(int a, int b) {
+		
+		int d;
+		int c=8+9;
+		return c;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		SonarTest st = new SonarTest();
+		System.out.println("Sum is: "+st.sum(10, 15));
+
+	}
+
+}
